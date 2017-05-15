4dfb49d72a6fb497ae040297e6e63fd9c6330cf4
popomore <sakura9515@gmail.com>
2017-05-16 00:40:47 +0800

    f

--- test/build.test.js
+++ test/build.test.js
@@ -1,6 +1,18 @@
 'use strict';
 
-describe('test/build.test.js', () => {
+const path = require('path');
+const coffee = require('coffee');
+const bin = path.join(__dirname, '../bin/doctools.js');
 
-  it('should work', () => {});
+
+describe.only('test/build.test.js', () => {
+
+  it('should work', function* () {
+    const cwd = path.join(__dirname, '../node_modules/egg');
+    yield coffee.fork(bin, [ 'build' ], { cwd })
+      .debug()
+      .expect('code', 0)
+      .end();
+  });
+
 });
