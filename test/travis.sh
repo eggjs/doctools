de2cc46cd36f4a16faf4ef357cd5676dff6ef4bc
popomore <sakura9515@gmail.com>
2017-05-17 16:15:19 +0800

    f

--- test/travis.sh
+++ test/travis.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-openssl aes-256-cbc -K $encrypted_54fd7b022195_key -iv $encrypted_54fd7b022195_iv -in test/deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $encrypted_28400b888db2_key -iv $encrypted_28400b888db2_iv -in test/deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
