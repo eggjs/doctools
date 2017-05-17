e55ba6b6e669394b009f557f30e8e9294eb518f6
popomore <sakura9515@gmail.com>
2017-05-17 16:44:54 +0800

    f

--- test/travis.sh
+++ test/travis.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-openssl aes-256-cbc -K $encrypted_28400b888db2_key -iv $encrypted_28400b888db2_iv -in deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $encrypted_28400b888db2_key -iv $encrypted_28400b888db2_iv -in test/deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 
 echo "" >> ~/.ssh/config
