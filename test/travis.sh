9fc053aaabc5cc3a417c0ce5b7c5239aea9e70b4
Haoliang Gao <sakura9515@gmail.com>
2017-05-17 19:41:37 +0800

    fix: use gh-pages 0.12.0 (#12)

--- test/travis.sh
+++ test/travis.sh
@@ -3,9 +3,6 @@
 openssl aes-256-cbc -K $encrypted_28400b888db2_key -iv $encrypted_28400b888db2_iv -in test/deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 
-eval `ssh-agent -s`
-ssh-add deploy_key
-
 echo "" >> ~/.ssh/config
 echo "Host github.com" >> ~/.ssh/config
 echo "  IdentityFile $PWD/deploy_key" >> ~/.ssh/config
