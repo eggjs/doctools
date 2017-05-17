766dac21197b825a35124cb733113ad173699501
popomore <sakura9515@gmail.com>
2017-05-17 18:18:38 +0800

    fix: use gh-pages 0.12.0

--- test/deploy.test.js
+++ test/deploy.test.js
@@ -4,16 +4,22 @@ const path = require('path');
 const coffee = require('coffee');
 const { rimraf, mkdirp } = require('mz-modules');
 const runscript = require('runscript');
+const assert = require('assert');
 const bin = path.join(__dirname, '../bin/doctools.js');
 
 describe('test/deploy.test.js', () => {
 
-  describe.skip('deploy', () => {
+  describe('deploy', () => {
     const fixtures = path.join(__dirname, 'fixtures');
     const cwd = path.join(fixtures, 'doctools');
     before(function* () {
       yield mkdirp(cwd);
-      yield runscript('git clone -b test-branch --depth 1 git@github.com:eggjs/doctools.git', { cwd: fixtures });
+      yield runscript('git clone -b test-branch git@github.com:eggjs/doctools.git', { cwd: fixtures });
+      try {
+        yield runscript('git push origin :gh-pages', { cwd });
+      } catch (err) {
+        console.log('catch error %s, but ignore', err.message);
+      }
     });
     after(() => rimraf(cwd));
 
@@ -22,6 +28,10 @@ describe('test/deploy.test.js', () => {
         .debug()
         .expect('code', 0)
         .end();
+
+      yield runscript('git fetch', { cwd });
+      const stdio = yield runscript('git branch -r', { cwd, stdio: 'pipe' });
+      assert(stdio.stdout.toString().includes('origin/gh-pages'));
     });
   });
 });
