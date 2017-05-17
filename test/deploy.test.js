91da0f32dfd1a72ded7ee0d092f0706f173cc35a
popomore <sakura9515@gmail.com>
2017-05-17 17:26:46 +0800

    f

--- test/deploy.test.js
+++ test/deploy.test.js
@@ -4,7 +4,6 @@ const path = require('path');
 const coffee = require('coffee');
 const { rimraf, mkdirp } = require('mz-modules');
 const runscript = require('runscript');
-const assert = require('assert');
 const bin = path.join(__dirname, '../bin/doctools.js');
 
 describe('test/deploy.test.js', () => {
@@ -15,12 +14,6 @@ describe('test/deploy.test.js', () => {
     before(function* () {
       yield mkdirp(cwd);
       yield runscript('git clone -b test-branch --depth 1 git@github.com:eggjs/doctools.git', { cwd: fixtures });
-      try {
-        yield runscript('git remote -v', { cwd });
-        yield runscript('git push origin :gh-pages', { cwd });
-      } catch (e) {
-        console.log('catch error %s, but ignore', e.message);
-      }
     });
     after(() => rimraf(cwd));
 
@@ -29,10 +22,6 @@ describe('test/deploy.test.js', () => {
         .debug()
         .expect('code', 0)
         .end();
-
-      yield runscript('git fetch', { cwd });
-      const res = yield runscript('git branch -r', { cwd, stdio: 'pipe' });
-      assert(res.stdout.toString().includes('origin/gh-pages'));
     });
   });
 });
