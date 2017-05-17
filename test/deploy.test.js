95c5b6cd076e999b759bfcd58a0b35f25e6c58c9
popomore <sakura9515@gmail.com>
2017-05-17 13:17:59 +0800

    refactor: add more deploy info

--- test/deploy.test.js
+++ test/deploy.test.js
@@ -2,22 +2,36 @@
 
 const path = require('path');
 const coffee = require('coffee');
-const { rimraf } = require('mz-modules');
+const { rimraf, mkdirp } = require('mz-modules');
+const runscript = require('runscript');
+const assert = require('assert');
 const bin = path.join(__dirname, '../bin/doctools.js');
 
-describe.skip('test/deploy.test.js', () => {
+describe('test/deploy.test.js', () => {
 
   describe('deploy', () => {
-    const cwd = path.join(__dirname, 'fixtures/framework');
-    const target = path.join(cwd, 'run/doctools');
+    const fixtures = path.join(__dirname, 'fixtures');
+    const cwd = path.join(fixtures, 'doctools-test-repo');
     before(function* () {
+      yield mkdirp(cwd);
+      yield runscript('git clone git@github.com:eggjs/doctools-test-repo.git', { cwd: fixtures });
+      try {
+        yield runscript('git push origin :gh-pages', { cwd });
+      } catch (e) {
+        console.log('catch error %s, but ignore', e.message);
+      }
+    });
+    after(() => rimraf(cwd));
+
+    it('should work', function* () {
       yield coffee.fork(bin, [ 'deploy' ], { cwd })
         .debug()
         .expect('code', 0)
         .end();
-    });
-    after(() => rimraf(target));
 
-    it('should work', () => {});
+      yield runscript('git fetch', { cwd });
+      const res = yield runscript('git branch -r', { cwd, stdio: 'pipe' });
+      assert(res.stdout.toString().includes('origin/gh-pages'));
+    });
   });
 });
