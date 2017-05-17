8bf8fc0b96fc43c7ffbbdf508e4bcb9e541b0ff0
popomore <sakura9515@gmail.com>
2017-05-17 15:32:56 +0800

    f

--- test/travis.sh
+++ test/travis.sh
@@ -1,12 +1,6 @@
 #! /usr/bin/env bash
 
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_a62186272189_key"
-ENCRYPTED_IV_VAR="encrypted_a62186272189_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-# echo "key: $ENCRYPTED_KEY, iv: $ENCRYPTED_IV"
-openssl aes-256-cbc -K $encrypted_a599f9128306_key -iv $encrypted_a599f9128306_iv -in test/deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $encrypted_28400b888db2_key -iv $encrypted_28400b888db2_iv -in test/deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
